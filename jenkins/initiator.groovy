78836598a1973efbd7d34e1211c9f30ee766d06f
ivash1228 <ivash1228@gmail.com>
2025-07-30 13:56:28 -0400

    modify function

--- jenkins/initiator.groovy
+++ jenkins/initiator.groovy
@@ -1,17 +1,14 @@
-static def getInitiator(currentBuild) {
-    def cause = currentBuild.rawBuild.getCauses().find {
-        c -> c.class.simpleName == 'UserIdCause'
-    }
-    if (cause) {
-        return cause.getUserName()
-    }
-    def timerCause = currentBuild.rawBuild.getCauses().find {
-        c -> c.class.simpleName == 'TimerTriggerCause'
-    }
-    if (timerCause) {
-        return 'Timer/Scheduler'
+def getInitiator(currentBuild) {
+    for (cause in currentBuild.getBuildCauses()) {
+        if (cause._class == 'org.jenkinsci.plugins.workflow.cps.Cause$UserIdCause' ||
+                cause.shortDescription.contains('Started by user')) {
+            return cause.userName ?: cause.shortDescription
+        }
+        if (cause._class == 'hudson.triggers.TimerTrigger$TimerTriggerCause' ||
+                cause.shortDescription.contains('Started by timer')) {
+            return 'Timer/Scheduler'
+        }
     }
     return 'Automated Trigger'
 }
-
-return [ getInitiator: this.&getInitiator ]
+return this
