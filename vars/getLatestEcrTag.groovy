7ee5688748d3d7540b57839e14ad05e2d15b9157
ivash1228 <ivash1228@gmail.com>
2025-08-14 09:38:12 -0400

    fixed back

--- vars/getLatestEcrTag.groovy
+++ vars/getLatestEcrTag.groovy
@@ -8,65 +8,24 @@
 
 def call(String repoName, String region = env.AWS_REGION) {
 
-    def arn = sh(
-            script: "aws sts get-caller-identity --query Arn --output text",
-            returnStdout: true
+    def latestTag = sh(
+        script: """
+            set -eo pipefail
+                aws ecr describe-images \
+                    --repository-name "${repoName}" \
+                    --region "${region}" \
+                    --registry-id "214404897309" \
+                    --no-paginate \
+                    --query 'sort_by(imageDetails,&imagePushedAt)[-1].imageTags[0]' \
+                    --output text
+        """,
+        returnStdout: true
     ).trim()
-    def isTerraformEnvRole = arn.contains(":assumed-role/npgw-jenkins-terraform-env-role/")
 
-    String latestTag
-
-    if (isTerraformEnvRole) {
-        latestTag = sh(
-            script: """
-            set -eo pipefail
-            aws ecr list-images \
-              --repository-name "${repoName}" \
-              --region "${region}" \
-              --registry-id "214404897309" \
-              --filter tagStatus=TAGGED \
-              --no-paginate \
-              --query 'imageIds[*].imageTag' \
-              --output text | tr '\\t' '\\n' | sort -r | head -n 1
-            """,
-            returnStdout: true
-        ).trim()
-    } else {
-        latestTag = sh(
-            script: """
-            set -eo pipefail
-            aws ecr describe-images \
-              --repository-name "${repoName}" \
-              --region "${region}" \
-              --registry-id "214404897309" \
-              --no-paginate \
-              --query 'sort_by(imageDetails,&imagePushedAt)[-1].imageTags[0]' \
-              --output text
-            """,
-            returnStdout: true
-        ).trim()
+    if (!latestTag || latestTag == 'None') {
+        error "[getLatestEcrTag] No image tags found in ${repoName}"
     }
-
-
-//
-//    def latestTag = sh(
-//        script: """
-//            set -eo pipefail
-//                aws ecr describe-images \
-//                    --repository-name "${repoName}" \
-//                    --region "${region}" \
-//                    --registry-id "214404897309" \
-//                    --no-paginate \
-//                    --query 'sort_by(imageDetails,&imagePushedAt)[-1].imageTags[0]' \
-//                    --output text
-//        """,
-//        returnStdout: true
-//    ).trim()
-//
-//    if (!latestTag || latestTag == 'None') {
-//        error "[getLatestEcrTag] No image tags found in ${repoName}"
-//    }
-//    echo "Resolved latest tag: ${latestTag}"
+    echo "Resolved latest tag: ${latestTag}"
 
     return latestTag
 }
