7cd48d54387c7b2d11beba6a62a1c18fa5409a69
ivash1228 <ivash1228@gmail.com>
2025-08-26 20:10:50 -0400

    some typo fix

--- vars/branchValidator.groovy
+++ vars/branchValidator.groovy
@@ -1,17 +1,17 @@
 def branchExists(String repo, String branch) {
-    withCredentials([string(credentialsId: 'GH_TOKEN', variable: 'GH_TOKEN')]) {
-        return (sh(
-            script: "gh api --silent /repos/NPGW/${repo}/git/ref/heads/${branch}",
-            returnStatus: true
-        ) == 0)
-    }
+    return (sh(
+        script: "gh api --silent /repos/NPGW/${repo}/git/ref/heads/${branch}",
+        returnStatus: true
+    ) == 0)
 }
 
 def returnShaIfBranchExists(String repo, String branch) {
-    withCredentials([string(credentialsId: 'GH_TOKEN', variable: 'GH_TOKEN')]) {
+    try {
         return sh(
-            script: "gh api /repos/NPGW/${repo}/git/ref/heads/${branch} --jq .object.sha",
-            returnStdout:true
+                script: "gh api /repos/NPGW/${repo}/git/ref/heads/${branch} --jq .object.sha",
+                returnStdout:true
         ).trim()
+    } catch (ignored) {
+        return null
     }
 }
