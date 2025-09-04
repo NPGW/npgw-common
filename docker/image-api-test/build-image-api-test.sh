0deb1e5f5dd02dbac1ed5a3185036c141b72c331
ivash1228 <ivash1228@gmail.com>
2025-09-04 13:03:03 -0400

    moved docker folder and deleted repos we don't need

--- docker/image-api-test/build-image-api-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../../
-docker build -t api-test:latest -f ./docker/image-api-test/Dockerfile .
--- docker/image-load-test/build-image-load-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../../
-docker build -t load-test:latest -f ./docker/image-load-test/Dockerfile .
